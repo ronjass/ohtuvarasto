4edd67cd331a2ede5bd9f1a3577efea3e4bb907b
Ronja <ronja.sonninen@helsinki.fi>
2025-11-05 12:36:17 +0200

    viikko 2 teht 6 tehty: pylint otettu käyttöön

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,9 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(tilavuus, 0.0)
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+        self.saldo = min(max(alku_saldo, 0.0), self.tilavuus)
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
